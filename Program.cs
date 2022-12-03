f2f3de59d707688800e1db7e74772122251bd634
Alexadr <sanz76@.mail.ru>
2022-12-03 15:08:41 +0300

    DZ_6

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число:");
+          int m = Convert.ToInt32(Console.ReadLine());
+
+            if (m % 2 == 1)
+            {
+                Console.WriteLine("Число " + m + " является: НЕЧЁТНЫМ");
+            }
+            else
+            {
+                Console.WriteLine("Число " + m + " является: ЧЁТНЫМ");
+            }
